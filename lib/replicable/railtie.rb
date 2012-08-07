8eaac059a2012f092ac0fa550e6a3ea7bf90eed6
Nicolas Viennot <nicolas@viennot.biz>
2012-08-07 17:17:04 -0400

    rails pub/sub loader

--- lib/replicable/railtie.rb
+++ lib/replicable/railtie.rb
@@ -1,5 +1,28 @@
 module Replicable
   class Railtie < Rails::Railtie
     rake_tasks { load 'replicable/railtie/replicate.rake' }
+
+    initializer 'load replicable' do
+      # TODO clean that up
+      config.after_initialize do
+        Dir[Rails.root.join('app', 'publishers', '**_publisher.rb')].map do |file|
+          file.split('/')[-1].split('.')[0].camelize.constantize
+        end
+        ActionDispatch::Reloader.to_prepare do
+          Dir[Rails.root.join('app', 'publishers', '**_publisher.rb')].map do |file|
+            file.split('/')[-1].split('.')[0].camelize.constantize
+          end
+        end
+
+        Dir[Rails.root.join('app', 'subscribers', '**_subscriber.rb')].map do |file|
+          file.split('/')[-1].split('.')[0].camelize.constantize
+        end
+        ActionDispatch::Reloader.to_prepare do
+          Dir[Rails.root.join('app', 'subscribers', '**_subscriber.rb')].map do |file|
+            file.split('/')[-1].split('.')[0].camelize.constantize
+          end
+        end
+      end
+    end
   end
 end
