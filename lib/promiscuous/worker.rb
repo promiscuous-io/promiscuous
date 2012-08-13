a92ca05715778aa2927d874adb213e88025b3564
Nicolas Viennot <nicolas@viennot.biz>
2012-08-13 15:55:01 -0400

    Mongoid unit of work wrapper

--- lib/promiscuous/worker.rb
+++ lib/promiscuous/worker.rb
@@ -1,32 +1,43 @@
 module Promiscuous
   module Worker
-    def self.replicate
-      stop = false
+    mattr_accessor :stop
 
-      unless ENV['TEST_ENV']
-        %w(SIGTERM SIGINT).each do |signal|
-          Signal.trap(signal) do
-            stop = true
-            EM.stop
-            Promiscuous.info "exiting gracefully"
-          end
-        end
-      end
+    def self.replicate
+      self.stop = false
+      self.trap_signals unless ENV['TEST_ENV']
 
       Promiscuous::AMQP.subscribe(subscribe_options) do |metadata, payload|
         begin
-          unless stop
+          unless self.stop
             Promiscuous.info "[receive] #{payload}"
-            Promiscuous::Subscriber.process(JSON.parse(payload))
+            self.mongoid_wrapper { Promiscuous::Subscriber.process(JSON.parse(payload)) }
             metadata.ack
           end
         rescue Exception => e
           e = Promiscuous::Subscriber::Error.new(e, payload)
 
-          stop = true
+          self.stop = true
           Promiscuous::AMQP.disconnect
           Promiscuous.error "[receive] FATAL #{e}"
           Promiscuous::Config.error_handler.call(e) if Promiscuous::Config.error_handler
+        end
+      end
+    end
+
+    def self.mongoid_wrapper
+      if defined?(Mongoid)
+        Mongoid.unit_of_work { yield }
+      else
+        yield
+      end
+    end
+
+    def self.trap_signals
+      %w(SIGTERM SIGINT).each do |signal|
+        Signal.trap(signal) do
+          self.stop = true
+          EM.stop
+          Promiscuous.info "exiting gracefully"
         end
       end
     end
