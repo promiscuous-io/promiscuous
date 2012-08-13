fe867a5d52fefac37e5555fa9ad3745d648f3daa
Nicolas Viennot <nicolas@viennot.biz>
2012-08-13 11:34:08 -0400

    Config fix

--- lib/promiscuous/config.rb
+++ lib/promiscuous/config.rb
@@ -3,13 +3,14 @@ module Promiscuous
     mattr_accessor :app, :logger, :error_handler, :backend, :server_uri, :queue_options
 
     def self.backend=(value)
-      @@backend = "Promiscuous::AMQP::#{value.to_s.camelize.gsub(/amqp/, 'AMQP')}".constantize
+      @@backend = "Promiscuous::AMQP::#{value.to_s.camelize.gsub(/amqp/, 'AMQP')}".constantize unless value.nil?
     end
 
     def self.configure(&block)
       class_variables.each { |var| class_variable_set(var, nil) }
 
       block.call(self)
+
       self.backend ||= defined?(EventMachine) && EventMachine.reactor_running? ? :rubyamqp : :bunny
       self.logger ||= defined?(Rails) ? Rails.logger : Logger.new(STDERR).tap { |l| l.level = Logger::WARN }
       self.queue_options ||= {:durable => true, :arguments => {'x-ha-policy' => 'all'}}
