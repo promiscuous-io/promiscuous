1fe0c285ce571de5007ff222592e2dab8be68dff
Nicolas Viennot <nicolas@viennot.biz>
2012-10-03 12:19:34 -0400

    removed useless line

--- spec/support/observers.rb
+++ spec/support/observers.rb
@@ -1,8 +1,6 @@
 module ObserversHelper
   def load_observers
     define_constant(:ModelObserver, Promiscuous::Observer) do
-      include ActiveSupport::Callbacks
-
       attr_accessor :id
       attr_accessor :field_1
       attr_accessor :field_2
