0d0ac9ab114bf22ab57eea8949eaa52bc6d1fc87
Nicolas Viennot <nicolas@viennot.biz>
2013-02-01 15:38:40 -0500

    Enforce mongoid version

--- lib/promiscuous/publisher/mongoid.rb
+++ lib/promiscuous/publisher/mongoid.rb
@@ -8,6 +8,7 @@ class Promiscuous::Publisher::Mongoid < Promiscuous::Publisher::Base
   include Promiscuous::Publisher::AMQP
 
   def self.publish(options)
+    check_mongoid_version
     super
 
     if klass.embedded?
@@ -18,5 +19,15 @@ class Promiscuous::Publisher::Mongoid < Promiscuous::Publisher::Base
     end
 
     setup_class_binding
+  end
+
+  def self.check_mongoid_version
+    unless Gem.loaded_specs['mongoid'].version >= Gem::Version.new('3.0.19')
+      raise "mongoid > 3.0.19 please"
+    end
+
+    unless Gem.loaded_specs['moped'].version >= Gem::Version.new('1.3.2')
+      raise "moped > 1.3.2 please"
+    end
   end
 end
