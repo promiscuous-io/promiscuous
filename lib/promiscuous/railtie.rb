a1096379d8c8753fc2c7092c2009a24a2f2e8626
Nicolas Viennot <nicolas@viennot.biz>
2013-02-22 05:24:48 -0500

    Rails Controller wrapper

--- lib/promiscuous/railtie.rb
+++ lib/promiscuous/railtie.rb
@@ -1,8 +1,17 @@
 class Promiscuous::Railtie < Rails::Railtie
+  module TransactionMiddleware
+    def process_action(*args)
+      Promiscuous.transaction("#{self.class.name}/#{self.action_name}") do
+        super
+      end
+    end
+  end
+
   initializer 'load promiscuous' do
     config.after_initialize do
       Promiscuous::Config.configure unless Promiscuous::Config.configured?
       Promiscuous::Loader.prepare
+      ActionController::Base.__send__(:include, TransactionMiddleware)
 
       ActionDispatch::Reloader.to_prepare do
         Promiscuous::Loader.prepare
