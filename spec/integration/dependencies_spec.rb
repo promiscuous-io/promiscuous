ea25fd34a1eb7c4cca9ec6c02d227a0a1a7dc8e0
Nicolas Viennot <nicolas@viennot.biz>
2013-02-27 23:07:17 -0500

    colon value test

--- spec/integration/dependencies_spec.rb
+++ spec/integration/dependencies_spec.rb
@@ -87,6 +87,24 @@ if ORM.has(:mongoid)
       end
     end
 
+    context 'when using : in the value' do
+      it 'replicates' do
+        Promiscuous.transaction do
+          pub = PublisherModel.create(:field_1 => ':hi')
+          eventually { SubscriberModel.first.field_1.should == ':hi' }
+          pub.update_attributes(:field_2 => ':hel:lo:')
+          eventually { SubscriberModel.first.field_2.should == ':hel:lo:' }
+          pub.update_attributes(:field_3 => 'hello:')
+          eventually { SubscriberModel.first.field_3.should == 'hello:' }
+          pub.update_attributes(:field_1 => ':')
+          eventually { SubscriberModel.first.field_1.should == ':' }
+
+          dep = pub.promiscuous.tracked_dependencies.first
+          Promiscuous::Redis.get(dep.key(:sub).for(:redis)).to_i.should == 4
+        end
+      end
+    end
+
     context 'when the publisher fails' do
       it 'replicates' do
         Promiscuous.transaction do
