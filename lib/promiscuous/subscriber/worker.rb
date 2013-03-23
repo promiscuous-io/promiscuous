5082feca72736445ba7faf65df02bc31987f13a0
Nicolas Viennot <nicolas@viennot.biz>
2013-03-23 01:13:36 -0400

    Configure num workers with THREADS env

--- lib/promiscuous/subscriber/worker.rb
+++ lib/promiscuous/subscriber/worker.rb
@@ -2,7 +2,7 @@ class Promiscuous::Subscriber::Worker < Celluloid::SupervisionGroup
   extend Promiscuous::Autoload
   autoload :Message, :Pump, :MessageSynchronizer, :Runner, :Stats, :Recorder
 
-  pool      Runner,              :as => :runners, :size => 10
+  pool      Runner,              :as => :runners, :size => ENV['THREADS'].try(:to_i) || 10
   supervise MessageSynchronizer, :as => :message_synchronizer
   supervise Pump,                :as => :pump
   supervise Stats,               :as => :stats
