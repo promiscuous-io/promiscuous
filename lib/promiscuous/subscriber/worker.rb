c5265049f647537bd41f5bdc292db05e9476387a
Nicolas Viennot <nicolas@viennot.biz>
2013-03-24 21:52:03 -0400

    Support single threading

--- lib/promiscuous/subscriber/worker.rb
+++ lib/promiscuous/subscriber/worker.rb
@@ -2,7 +2,14 @@ class Promiscuous::Subscriber::Worker < Celluloid::SupervisionGroup
   extend Promiscuous::Autoload
   autoload :Message, :Pump, :MessageSynchronizer, :Runner, :Stats, :Recorder
 
-  pool      Runner,              :as => :runners, :size => ENV['THREADS'].try(:to_i) || 10
+  NUM_THREADS = ENV['THREADS'].try(:to_i) || 10
+
+  if NUM_THREADS > 1
+    pool      Runner, :as => :runners, :size => NUM_THREADS
+  else
+    supervise Runner, :as => :runners
+  end
+
   supervise MessageSynchronizer, :as => :message_synchronizer
   supervise Pump,                :as => :pump
   supervise Stats,               :as => :stats
