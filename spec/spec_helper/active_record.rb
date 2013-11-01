7d6708142b59552bdd6980a9fbde22e69f5a8498
Nicolas Viennot <nicolas@viennot.biz>
2013-10-31 21:08:13 -0400

    Auto create database

--- spec/spec_helper/active_record.rb
+++ spec/spec_helper/active_record.rb
@@ -5,18 +5,24 @@ if ENV['LOGGER_LEVEL']
   ActiveRecord::Base.logger.level = ENV['LOGGER_LEVEL'].to_i
 end
 
-ActiveRecord::Base.establish_connection(
+db_settings = {
   :adapter  => RUBY_PLATFORM == "java" ? "jdbcpostgresql" : "postgresql",
   :database => "promiscuous",
-  :username => ENV["TRAVIS"] ? "postgres" : "promiscuous",
-  :password => ENV["TRAVIS"] ?        nil : "promiscuous",
+  :username => "postgres",
+  :password => nil,
   :encoding => "utf8",
   :pool => 20,
-)
+}
 
+ActiveRecord::Base.establish_connection(db_settings.merge('database' => 'postgres'))
 ActiveRecord::Base.connection.execute("select gid from pg_prepared_xacts").column_values(0).each do |xid|
   ActiveRecord::Base.connection.execute("ROLLBACK PREPARED '#{xid}'")
 end
+
+ActiveRecord::Base.connection.drop_database(db_settings[:database]) rescue nil
+ActiveRecord::Base.connection.create_database(db_settings[:database])
+ActiveRecord::Base.establish_connection(db_settings)
+
 
 class PromiscuousMigration < ActiveRecord::Migration
   def change
