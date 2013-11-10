85f60a20df0823136d57c8c8e9737782ff9d9f2a
Kareem Kouddous <kareemknyc@gmail.com>
2013-11-10 02:16:21 -0500

    Travis fixes

--- spec/support/_coverage.rb
+++ spec/support/_coverage.rb
@@ -1,17 +1,19 @@
-require 'simplecov'
+unless RUBY_PLATFORM == 'java'
+  require 'simplecov'
 
-if ENV['TRAVIS']
-  require 'coveralls'
-  SimpleCov.formatter = Coveralls::SimpleCov::Formatter
-end
+  if ENV['TRAVIS']
+    require 'coveralls'
+    SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+  end
 
-SimpleCov.start do
-  add_filter '/spec/'
-  add_filter 'debug.rb'
+  SimpleCov.start do
+    add_filter '/spec/'
+    add_filter 'debug.rb'
 
-  add_group 'Controllers',  'lib/promiscuous'
-  add_group 'AMQP Drivers', 'lib/promiscuous/amqp'
-  add_group 'CLI',          'lib/promiscuous/cli'
-  add_group 'Publisher',    'lib/promiscuous/publisher'
-  add_group 'Subscriber',   'lib/promiscuous/subscriber'
+    add_group 'Controllers',  'lib/promiscuous'
+    add_group 'AMQP Drivers', 'lib/promiscuous/amqp'
+    add_group 'CLI',          'lib/promiscuous/cli'
+    add_group 'Publisher',    'lib/promiscuous/publisher'
+    add_group 'Subscriber',   'lib/promiscuous/subscriber'
+  end
 end
