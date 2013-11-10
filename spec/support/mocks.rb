3c3720d0f39fd493302e02dab5d3991ae87c6e62
Nicolas Viennot <nicolas@viennot.biz>
2013-11-10 00:12:22 -0500

    Only mock with BSON for mongoid in tests

--- spec/support/mocks.rb
+++ spec/support/mocks.rb
@@ -3,7 +3,7 @@ module MocksHelper
     define_constant :MockModel do
       include Promiscuous::Publisher::Model::Mock
       publish :field_1, :field_2, :field_3
-      mock    :id => :bson
+      mock    :id => :bson if ORM.has(:mongoid)
     end
   end
 end
