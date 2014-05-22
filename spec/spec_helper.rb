2098c9152445e4728cd9b43f2f02a2d17fd95a2f
Kareem Kouddous <kareemk@crowdtap.com>
2014-05-22 14:32:36 -0400

    Retry more for travis

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -21,7 +21,7 @@ if ENV['TRAVIS']
   require 'rspec/retry'
   RSpec.configure do |config|
     config.verbose_retry = true
-    config.default_retry_count = 3
+    config.default_retry_count = 5
   end
 end
 
