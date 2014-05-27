5cf6b3678553bd0865ba9340c1f279a53760b2df
Kareem Kouddous <kareemk@crowdtap.com>
2014-05-27 18:25:55 -0400

    Lazily check for _promiscuous table

--- lib/promiscuous/publisher/transport/persistence/active_record.rb
+++ lib/promiscuous/publisher/transport/persistence/active_record.rb
@@ -1,17 +1,7 @@
 class Promiscuous::Publisher::Transport::Persistence::ActiveRecord
-  def initialize
-    unless connection.table_exists?(table)
-      raise <<-help
-        Promiscuous requires the following migration to be run:
-          create_table :_promiscuous do |t|
-            t.string    :batch
-            t.timestamp :at, :default => :now
-          end
-      help
-    end
-  end
-
   def save(batch)
+    check_schema
+
     q = "INSERT INTO #{table} (\"batch\") " +
       "VALUES ('#{batch.dump}') RETURNING id"
 
@@ -21,6 +11,8 @@ class Promiscuous::Publisher::Transport::Persistence::ActiveRecord
   end
 
   def expired
+    check_schema
+
     q = "SELECT id, p.batch FROM #{table} p " +
       "WHERE at < current_timestamp - #{Promiscuous::Config.recovery_timeout} * INTERVAL '1 second'"
 
@@ -28,12 +20,30 @@ class Promiscuous::Publisher::Transport::Persistence::ActiveRecord
   end
 
   def delete(batch)
+    check_schema
+
     q = "DELETE FROM #{table} WHERE id = #{batch.id}"
 
     connection.exec_query(q, 'Promiscuous Recovery Delete')
   end
 
   private
+
+  def check_schema
+    return if @schema_checked
+
+    unless connection.table_exists?(table)
+      raise <<-help
+        Promiscuous requires the following migration to be run:
+          create_table :_promiscuous do |t|
+            t.string    :batch
+            t.timestamp :at, :default => :now
+          end
+      help
+    end
+
+    @schema_checked = true
+  end
 
   def connection
     ActiveRecord::Base.connection
