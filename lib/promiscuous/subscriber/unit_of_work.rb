34d2cc3cbc3fb812bf464df3953ff58acfce82c0
Kareem Kouddous <kareemk@crowdtap.com>
2015-01-28 16:51:30 -0500

    ALL PASSING!!!

--- lib/promiscuous/subscriber/unit_of_work.rb
+++ lib/promiscuous/subscriber/unit_of_work.rb
@@ -40,8 +40,9 @@ class Promiscuous::Subscriber::UnitOfWork
       on_message
     rescue Exception => e
       Promiscuous::Config.error_notifier.call(e)
-      message.nack
+      # message.nack
       raise e if Promiscuous::Config.test_mode
+      sleep Promiscuous::Config.error_ttl / 1000.0; retry
     end
   end
 
